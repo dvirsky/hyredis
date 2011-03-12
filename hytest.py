20ee26ea2122aa40ccbb7d3e1ad48ba09cf87dd9
dvirsky <dvir@doit9.com>
2011-03-12 21:04:44 +0200

    Added support for binary module pipelining

--- hytest.py
+++ hytest.py
@@ -1,4 +1,13 @@
 import hyredis
 h = hyredis.Redis()
-print "Set: %s" % h.command('set', ('foo', 'bar'))
-print "Get: %s" % h.command('get', ('foo',))
+print "Set: %s" % h.command(('set', 'foo', 'bar'))
+print "Get: %s" % h.command(('get', 'foo',))
+print "Testing transaction..."
+p = hyredis.Redis(pipeline = True)
+assert(p)
+p.command(('set', 'bar', 'baz'))
+p.command(('get', 'bar',))
+p.command(('get', 'foo',))
+r = p.execute()
+assert(r)
+print "Transactino output: %s" % r
